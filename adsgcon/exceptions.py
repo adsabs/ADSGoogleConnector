2d9befc87d8bb5a1e1cf4644acd4839237f2b3c3
Matthew Templeton <matthew.templeton@cfa.harvard.edu>
2023-01-18 18:47:06 -0500

    Initial commit with service acct authentication  	modified:   .gitignore  	deleted:    LICENSE  	new file:   adsgcon/exceptions.py  	new file:   adsgcon/gmanager.py  	new file:   requirements.txt  	new file:   setup.py

---
+++ adsgcon/exceptions.py
@@ -0,0 +1,26 @@
+class BadAuthtypeException(Exception):
+    pass
+
+
+class GoogleAuthException(Exception):
+    pass
+
+
+class GoogleUploadException(Exception):
+    pass
+
+
+class MissingFileException(Exception):
+    pass
+
+
+class GoogleDownloadException(Exception):
+    pass
+
+
+class GoogleSheetExportException(Exception):
+    pass
+
+
+class GoogleReparentException(Exception):
+    pass
